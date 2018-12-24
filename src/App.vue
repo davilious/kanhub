57ae31098131d84b78868865bc1ee9ddaee52059
Dávila <davila@bebanjo.com>
2018-12-24 16:28:03 +0100

    Add first components

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,48 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="container" v-show="!loading">
+    <div class="column column--todo">
+      <Card v-for="(card, index) in cardsBy('todo')" :payload="card" :key="index"></Card>
+    </div>
+    <div class="column column--development">
+      <Card v-for="(card, index) in cardsBy('development')" :payload="card" :key="index"></Card>
+    </div>
+    <div class="column column--done">
+      <Card v-for="(card, index) in cardsBy('done')" :payload="card" :key="index"></Card>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Card from './components/Card.vue';
 
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  name: 'Kanhub',
+  data() {
+    return {
+      cards: null,
+      loading: true
+    }
+  },
+  mounted () {
+    axios
+      .get('http://localhost:8000/api/all')
+      .then(response => {
+        this.cards = response.data
+      })
+      .finally(() => this.loading = false)
+  },
+  components: { Card },
+  methods: {
+    cardsBy(state) {
+      if (this.cards) {
+        return this.cards.filter(card => card.state === state)
+      }
+    }
   }
 }
 </script>
 
-<style>
+<style scoped>
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -25,4 +51,27 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+
+.container {
+  display: flex;
+  width: 100%;
+}
+
+.column {
+  flex-basis: 33%;
+  padding: 10px;
+}
+
+.column.column--todo {
+  order: 1;
+}
+
+.column.column--development {
+  order: 2;
+}
+
+.column.column--done {
+  order: 3;
+}
 </style>
+
---
+++ src/components/Card.vue
@@ -0,0 +1,32 @@
+<template>
+  <div v-bind:class="generateClassByState(payload.state)">
+    <span>{{ payload.title }}</span>
+    <span>{{ payload.state }}</span>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Card',
+
+    props: {
+      payload: Object
+    },
+
+    methods: {
+      generateClassByState(state) {
+        return `card card--${state}`
+      }
+    }
+  }
+</script>
+
+<style>
+  .card {
+    display: flex;
+    flex-direction: column;
+    height: 150px;
+    border: 1px solid orange;
+    margin-bottom: 10px;
+  }
+</style>
